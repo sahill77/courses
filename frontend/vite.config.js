0948dafc33328fced85130fdd31ab915787a79c5
Sahil <sahilvanzara49@gmail.com>
2026-03-11 16:57:25 +0530

    target url changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://courses-jet-iota.vercel.app/api",
+        target: "https://course-tau-bice.vercel.app/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },  
