25d018a8a44822037dc69f782f531df7a00abc1c
Sahil <sahilvanzara49@gmail.com>
2026-03-11 15:36:39 +0530

    comm 2

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -28,7 +28,7 @@ router.post("/login", async (req, res) => {
     }
     const token = jwt.sign(
       { id: user._id, role: user.role },
-      process.env.JWT_SECRET,
+      "supersecretkey123",
       { expiresIn: "1d" },
     );
     res.send({
