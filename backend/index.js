d154487db44f57e7a23e2186a42a631665a73138
sahill77 <sahilvanzara49@gmail.com>
2026-03-17 21:15:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://ocm-1.vercel.app",
+    origin: "*",
     methods: ["GET","POST","PUT","DELETE"],
     credentials:true
   }));
