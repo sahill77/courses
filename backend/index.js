5f1132c21ff3eb9277c114fc10cc19eeefe6eefe
Sahil <sahilvanzara49@gmail.com>
2026-03-18 12:55:08 +0530

    origiin url changed

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://courses-f.vercel.app",
     methods: ["GET","POST","PUT","DELETE"],
     credentials:true
   }));
