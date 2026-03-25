a5330b27709ca4ee92fbb56848d1f7f97b7e5f47
Sahil <sahilvanzara49@gmail.com>
2026-03-25 16:49:55 +0530

    date a time

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://courses-fr.vercel.app",
+    credentials: true,
+  })
+);
 app.get("/", (req, res) => {
   res.send("app is now running");
 });
